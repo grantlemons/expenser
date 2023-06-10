29191818697f6b1dc5cec9574bf05d7e5b92c22e
Grant Lemons <grant0lemons@gmail.com>
2023-06-10 18:53:00 -0500

    scaffold project filestructure

---
+++ src/server/server.rs
@@ -0,0 +1,12 @@
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+#[allow(unused_imports)]
+use invoice::*;
+
+fn main() {
+    println!("server");
+}
