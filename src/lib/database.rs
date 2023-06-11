7a9093c8610b952c6cecbd67bd5b2c332597d7f9
Grant Lemons <grant0lemons@gmail.com>
2023-06-11 09:43:45 -0500

    line item model

--- src/lib/database.rs
+++ src/lib/database.rs
@@ -4,7 +4,7 @@ use diesel::prelude::*;
 use dotenvy::dotenv;
 
 pub fn establish_connection() -> Result<PgConnection> {
-    dotenv().ok();
+    let _ = dotenv();
 
     let database_url = std::env::var("DATABASE_URL").context("DATABASE_URL not set")?;
     let conn = PgConnection::establish(&database_url).context("Error connecting to database")?;
--- src/lib/models.rs
+++ src/lib/models.rs
@@ -16,11 +16,38 @@ pub struct User {
 #[derive(Insertable, Debug, PartialEq)]
 #[diesel(table_name = users)]
 pub struct NewUser<'a> {
-    pub user_id: i64,
     pub username: &'a str,
     pub email: &'a str,
     pub profile_picture: Option<&'a [u8]>,
     pub password_hash: &'a str,
+}
+
+impl User {
+    pub fn new(conn: &mut PgConnection, username: &str, email: &str, password: &str) -> Self {
+        use argon2::{
+            password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+            Argon2,
+        };
+
+        let salt = SaltString::generate(&mut OsRng);
+        let argon2 = Argon2::default();
+        let hash = argon2
+            .hash_password(password.as_bytes(), &salt)
+            .expect("Hashing function failed")
+            .to_string();
+
+        let new_user = NewUser {
+            username,
+            email,
+            profile_picture: None,
+            password_hash: &hash,
+        };
+        diesel::insert_into(users::table)
+            .values(&new_user)
+            .returning(Self::as_returning())
+            .get_result(conn)
+            .expect("Error creating new user")
+    }
 }
 
 #[derive(Queryable, Selectable, Identifiable, Debug, PartialEq)]
@@ -35,7 +62,6 @@ pub struct Invoice {
 #[derive(Insertable, Debug, PartialEq)]
 #[diesel(table_name = invoices)]
 pub struct NewInvoice {
-    pub invoice_id: i64,
     pub owner_id: i64,
 }
 
@@ -54,7 +80,6 @@ pub struct InvoiceProof {
 #[diesel(belongs_to(Invoice))]
 #[diesel(table_name = invoice_proof)]
 pub struct NewInvoiceProof<'a> {
-    pub proof_id: i64,
     pub invoice_id: i64,
     pub data: &'a [u8],
 }
@@ -74,13 +99,34 @@ pub struct InvoicePermissions {
 }
 
 #[derive(Insertable, Associations, Debug, PartialEq)]
-#[diesel(belongs_to(User, foreign_key = borrower_id))]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 #[diesel(belongs_to(Invoice))]
 #[diesel(table_name = invoice_permissions)]
 pub struct NewInvoicePermissions {
-    pub access_id: i64,
     pub borrower_id: i64,
     pub invoice_id: i64,
     pub read_access: bool,
     pub write_access: bool,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(primary_key(id))]
+#[diesel(belongs_to(Invoice))]
+#[diesel(table_name = invoice_line_items)]
+pub struct InvoiceLineItem {
+    id: i64,
+    invoice_id: i64,
+    item_name: String,
+    item_price_usd: diesel::data_types::Cents,
+}
+
+#[derive(Insertable, Associations, Debug, PartialEq)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(belongs_to(Invoice))]
+#[diesel(table_name = invoice_line_items)]
+pub struct NewInvoiceLineItem {
+    invoice_id: i64,
+    item_name: String,
+    item_price_usd: diesel::data_types::Cents,
 }
