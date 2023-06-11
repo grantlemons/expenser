6ae1adb37724449a3fb09f4560679236a42cdb58
Grant Lemons <grant0lemons@gmail.com>
2023-06-10 23:56:05 -0500

    create models for rust representation of database data

--- src/lib/models.rs
+++ src/lib/models.rs
@@ -1,11 +1,48 @@
+use crate::schema::*;
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = crate::schema::posts)]
+#[derive(Queryable, Selectable, Identifiable, Debug, PartialEq)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct Post {
-    pub id: i32,
-    pub title: String,
-    pub body: String,
-    pub published: bool,
+#[diesel(primary_key(user_id))]
+#[diesel(table_name = users)]
+pub struct User {
+    pub user_id: i64,
+    pub username: String,
+    pub email: String,
+    pub profile_picture: Option<Vec<u8>>,
+    pub password_hash: String,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Debug, PartialEq)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(primary_key(invoice_id))]
+#[diesel(table_name = invoices)]
+pub struct Invoice {
+    pub invoice_id: i64,
+    pub owner_id: i64,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(primary_key(proof_id))]
+#[diesel(belongs_to(Invoice))]
+#[diesel(table_name = invoice_proof)]
+pub struct InvoiceProof {
+    pub proof_id: i64,
+    pub invoice_id: i64,
+    pub data: Vec<u8>,
+}
+
+#[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(primary_key(access_id))]
+#[diesel(belongs_to(User, foreign_key = borrower_id))]
+#[diesel(belongs_to(Invoice))]
+#[diesel(table_name = invoice_permissions)]
+pub struct InvoicePermissions {
+    pub access_id: i64,
+    pub borrower_id: i64,
+    pub invoice_id: i64,
+    pub read_access: bool,
+    pub write_access: bool,
 }
