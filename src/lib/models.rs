ba9b20521770f351b7fa77d642dcc73418e4e601
Grant Lemons <grant0lemons@gmail.com>
2023-06-11 00:04:47 -0500

    add insertable variants of models

--- src/lib/models.rs
+++ src/lib/models.rs
@@ -13,11 +13,28 @@ pub struct User {
     pub password_hash: String,
 }
 
+#[derive(Insertable, Debug, PartialEq)]
+#[diesel(table_name = users)]
+pub struct NewUser<'a> {
+    pub user_id: i64,
+    pub username: &'a str,
+    pub email: &'a str,
+    pub profile_picture: Option<&'a [u8]>,
+    pub password_hash: &'a str,
+}
+
 #[derive(Queryable, Selectable, Identifiable, Debug, PartialEq)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 #[diesel(primary_key(invoice_id))]
 #[diesel(table_name = invoices)]
 pub struct Invoice {
+    pub invoice_id: i64,
+    pub owner_id: i64,
+}
+
+#[derive(Insertable, Debug, PartialEq)]
+#[diesel(table_name = invoices)]
+pub struct NewInvoice {
     pub invoice_id: i64,
     pub owner_id: i64,
 }
@@ -33,6 +50,15 @@ pub struct InvoiceProof {
     pub data: Vec<u8>,
 }
 
+#[derive(Insertable, Associations, Debug, PartialEq)]
+#[diesel(belongs_to(Invoice))]
+#[diesel(table_name = invoice_proof)]
+pub struct NewInvoiceProof<'a> {
+    pub proof_id: i64,
+    pub invoice_id: i64,
+    pub data: &'a [u8],
+}
+
 #[derive(Queryable, Selectable, Identifiable, Associations, Debug, PartialEq)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 #[diesel(primary_key(access_id))]
@@ -40,6 +66,18 @@ pub struct InvoiceProof {
 #[diesel(belongs_to(Invoice))]
 #[diesel(table_name = invoice_permissions)]
 pub struct InvoicePermissions {
+    pub access_id: i64,
+    pub borrower_id: i64,
+    pub invoice_id: i64,
+    pub read_access: bool,
+    pub write_access: bool,
+}
+
+#[derive(Insertable, Associations, Debug, PartialEq)]
+#[diesel(belongs_to(User, foreign_key = borrower_id))]
+#[diesel(belongs_to(Invoice))]
+#[diesel(table_name = invoice_permissions)]
+pub struct NewInvoicePermissions {
     pub access_id: i64,
     pub borrower_id: i64,
     pub invoice_id: i64,
