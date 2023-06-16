0aa275a53165702ec8cd75aadc093ab626503380
Grant Lemons <grant0lemons@gmail.com>
2023-06-15 22:37:04 -0500

    logger default folder

--- backend/src/server/logger.rs
+++ backend/src/server/logger.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Result};
 
 /// Logger configuration using [`fern`]
 pub fn setup() -> Result<()> {
-    let base_dir = std::env::var("CARGO_MANIFEST_DIR").context("Unable to get manifest dir")?;
+    let base_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap_or("/".to_owned());
     let current_datetime = chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]");
     let file_name = format!("{base_dir}/logs/{current_datetime}.log");
 
