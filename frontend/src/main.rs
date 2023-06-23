265d87e71ec2fa37fd0a57081f295a50673acd45
Grant Lemons <glemons@headstorm.com>
2023-06-23 13:52:02 -0500

    create basic site to test leptos framework and trunk

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,21 @@
+use leptos::*;
+
 fn main() {
-    println!("Hello, world!");
+    leptos::mount_to_body(|cx| view! { cx, <App/> });
+}
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            {count}
+        </button>
+    }
 }
